dfafbe42f06e22d4de9a9ec1e449762bf6d59ac6
Mark Olliver <mark@digitalpatterns.io>
2019-07-19 10:28:03 +0100

    add start of deployment files

---
+++ healthcheck.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const options = {
+  'url': 'http://127.0.0.1:5000/_health',
+  'timeout': 2000,
+};
+
+request(options, (error, response, body) => {
+  if (response && response.statusCode === 200) {
+    // eslint-disable-next-line no-console
+    console.log(`STATUS: ${response.statusCode}`);
+    process.exit(0);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`ERROR: ${error}`);
+    process.exit(1);
+  }
+});
