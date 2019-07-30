b18ff7ef7f9f9a50f0969130fdd88c3f94acb73c
Pedro Miguel <pedro.miguel@live.co.uk>
2019-07-30 15:13:25 +0100

    Add functionality to allow data updates

--- app/db/utils.js
+++ app/db/utils.js
@@ -13,131 +13,141 @@ function viewFunctionArgsBuilder(body='') {
   return args ? `(${args})` : args;
 }
 
-function insertIntoStringBuilder(body='') {
-  let insertIntoStr = '';
+function insertIntoOptionsBuilder(body) {
   let columns = '';
   let values = '';
 
-  if (body) {
-    for (const key in body) {
-      columns += columns ? ',' : '';
-      columns += key;
-      values += values ? ',' : '';
+  for (const key in body) {
+    columns += columns ? ',' : '';
+    columns += key;
+    values += values ? ',' : '';
 
-      if (typeof(body[key]) === 'object') {
-        values += `'${JSON.stringify(body[key])}'`
-      } else {
-        values += `'${body[key]}'`;
-      }
-    }
-    insertIntoStr = `(${columns}) VALUES (${values})`;
-  }
-  return insertIntoStr;
-}
-
-function updateStringBuilder(body='') {
-  let updateStr = '';
-
-  if (body) {
-    for (const key in body) {
-      updateStr += updateStr ? ',' : '';
-
-      if (typeof(body[key]) === 'object') {
-        updateStr += `${key}=${JSON.stringify(body[key])}`;
-      } else {
-        updateStr += `${key}='${body[key]}'`;
-      }
+    if (typeof(body[key]) === 'object') {
+      values += `'${JSON.stringify(body[key])}'`
+    } else {
+      values += `'${body[key]}'`;
     }
   }
-  return updateStr;
+  return `(${columns}) VALUES (${values})`;
 }
 
-function queryBuilder(name, { id='', queryParams='', body='', method='', prefer='' }) {
-  let args = '';
-  let insertStr = '';
+function queryParamsBuilder({ queryParams, body='', name='' }) {
+  let options = '';
   let query = '';
-  let updateStr = '';
-  const returning = prefer ? '%20RETURNING *' : '';
 
-  if (!body && !queryParams) {
-    query = `SELECT * FROM ${name}`;
-  } else if (body && method === 'POST') {
-    insertStr = insertIntoStringBuilder(body);
-    query = `INSERT INTO ${name} ${insertStr}${returning}`;
-  } else if (body && method === 'PATCH') {
-    updateStr = updateStringBuilder(body);
-    query = `UPDATE ${name} SET ${updateStr} WHERE id=id${returning}`;
-  } else if (body && !queryParams) {
+  queryParams = queryParams.replace(/,&/g, ';%20AND%20');
+  queryParams = queryParams.replace(/&/g, ';&');
+  queryParams = queryParams.split(';');
+  queryParams.map((params) => {
     args = viewFunctionArgsBuilder(body);
-    query = `SELECT * FROM ${name}${args}`;
-  } else {
-    logger.info(`Query parameters received '${queryParams}'`);
+    params = params.replace('=', ' ').replace('.', ' ').split(' ');
+    let [field='', filter='', value=''] = params;
+    let isNull = false;
 
-    queryParams = queryParams.replace(/,&/g, ';AND%20');
-    queryParams = queryParams.replace(/&/g, ';&');
-    queryParams = queryParams.split(';');
-    queryParams.map((params) => {
-      args = viewFunctionArgsBuilder(body);
-      params = params.replace('=', ' ').replace('.', ' ').split(' ');
-      let [field='', filter='', value=''] = params;
-      let options = '';
-      let isNull = false;
+    if (!filter) {
+      return '';
+    }
 
-      if (!filter) {
-        return '';
-      }
+    if (filter !== 'in' && isNaN(value) && value !== 'null') {
+      value = `'${value}'`;
+    } else if (isNaN(value) && value === 'null') {
+      isNull = true;
+      value = value.toUpperCase();
+    }
 
-      if (filter !== 'in' && isNaN(value) && value !== 'null') {
-        value = `'${value}'`;
-      } else if (isNaN(value) && value === 'null') {
-        isNull = true;
-        value = value.toUpperCase();
-      }
+    if (field === 'select') {
+      field = field.toUpperCase();
+    } else if (filter === 'gt' && !isNull) {
+      filter = '>';
+    } else if (filter === 'gte' && !isNull) {
+      filter = '>=';
+    } else if (filter === 'lt' && !isNull) {
+      filter = '<';
+    } else if (filter === 'lte' && !isNull) {
+      filter = '<=';
+    } else if (filter === 'eq' && !isNull) {
+      filter = '=';
+    } else if (filter === 'eq' && isNull) {
+      filter = 'IS';
+    } else {
+      filter = 'IN';
+      value = value.replace('%28', '').replace('%29', '').replace(/%20/g, ' ');
 
-      if (!method && field === 'select') {
-        field = field.toUpperCase();
-      } else if (filter === 'gt' && !isNull) {
-        filter = '>';
-      } else if (filter === 'gte' && !isNull) {
-        filter = '>=';
-      } else if (filter === 'lt' && !isNull) {
-        filter = '<';
-      } else if (filter === 'lte' && !isNull) {
-        filter = '<=';
-      } else if (filter === 'eq' && !isNull) {
-        filter = '=';
-      } else if (filter === 'eq' && isNull) {
-        filter = 'IS';
+      let values = value.split(',');
+      value = values.map(val => val.trim());
+      value = `'${value.join("', '")}'`;
+      value = `(${value})`;
+    }
+
+    if (!value) {
+      filter = filter.replace(/,/g, ' ');
+      filter = filter.trim();
+      filter = filter.replace(/ /g, ', ');
+      query = `${field} ${filter} FROM ${name}${args}`;
+    } else {
+      if (query) {
+        query += query.includes('WHERE') ? `${field} ${filter} ${value}` : `%20WHERE ${field} ${filter} ${value}`;
       } else {
-        filter = 'IN';
-        value = value.replace('%28', '').replace('%29', '').replace(/%20/g, ' ');
-
-        let values = value.split(',');
-        value = values.map(val => val.trim());
-        value = `'${value.join("', '")}'`;
-        value = `(${value})`;
+        options += `${field} ${filter} ${value}`;
       }
-
-      if (!value) {
-        filter = filter.replace(/,/g, ' ');
-        filter = filter.trim();
-        filter = filter.replace(/ /g, ', ');
-        query = `${field} ${filter} FROM ${name}${args}`;
-      } else {
-        if (query) {
-          query += query.includes('WHERE') ? `%20${field} ${filter} ${value}` : `%20WHERE ${field} ${filter} ${value}`;
-        } else if (method === 'DELETE') {
-          query = `DELETE FROM ${name} WHERE ${field} ${filter} ${value}`
-        } else {
-          query = `SELECT * FROM ${name} WHERE ${field} ${filter} ${value}`
-        }
-      }
-    });
-  }
+    }
+  });
 
   query = query.replace(/%20/g, ' ');
   query = query.replace('&', '');
-  return query ? `${query};` : query;
+  query = query ? `${query};` : query;
+  options = options.replace(/%20/g, ' ');
+  return { query, options };
 }
 
-module.exports = queryBuilder;
+// Creates a SELECT querystring
+function selectQueryBuilder({ name, body='', queryParams='' }) {
+  if (!body && !queryParams) {
+    return `SELECT * FROM ${name};`;
+  } else if (body && !queryParams) {
+    const args = viewFunctionArgsBuilder(body);
+    return `SELECT * FROM ${name}${args};`;
+  } else {
+    const { query, options } = queryParamsBuilder({ name, queryParams, body });
+    return options ? `SELECT * FROM ${name} WHERE ${options};` : query;
+  }
+}
+
+// Creates a INSERT INTO querystring
+function insertIntoQueryBuilder({ name, body, prefer='' }) {
+  const returning = prefer ? ' RETURNING *' : '';
+  const options = insertIntoOptionsBuilder(body);
+  return `INSERT INTO ${name} ${options}${returning};`;
+}
+
+// Creates an UPDATE querystring
+function updateQueryBuilder({ name, body, id='', prefer='', queryParams='' }) {
+  let values = '';
+  const returning = prefer ? ' RETURNING *' : '';
+
+  for (const key in body) {
+    values += values ? ',' : '';
+
+    if (typeof(body[key]) === 'object') {
+      values += `${key}=${JSON.stringify(body[key])}`;
+    } else {
+      values += `${key}='${body[key]}'`;
+    }
+  }
+  queryParams = id ? `id=eq.${id}` : queryParams;
+  const { options } = queryParamsBuilder({ queryParams });
+  return values ? `UPDATE ${name} SET ${values} WHERE ${options}${returning};` : '';
+}
+
+// Creates a DELETE querystring
+function deleteQueryBuilder({ name, queryParams }) {
+  const { options } = queryParamsBuilder({ queryParams });
+  return `DELETE FROM ${name} WHERE ${options};`
+}
+
+module.exports = {
+  deleteQueryBuilder,
+  insertIntoQueryBuilder,
+  selectQueryBuilder,
+  updateQueryBuilder
+};
