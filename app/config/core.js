27cef376c7d3a362067b8a18e08b20b20e0ee591
Pedro Miguel <pedro.miguel@live.co.uk>
2019-08-19 11:21:28 +0100

    Update configuration settings

--- app/config/core.js
+++ app/config/core.js
@@ -1,27 +1,27 @@
 const db = {
   'protocol': process.env.DB_COP_PROTOCOL || 'postgres://',
-  'port': process.env.DB_COP_PORT || '5432',
+  'port': process.env.DB_COP_PORT || '5434',
   'hostname': process.env.DB_COP_HOSTNAME || 'localhost',
   'options': process.env.DB_COP_OPTIONS || '',
-  'name': process.env.DB_COP_OPERATION_DBNAME || 'operation',
-  'username': process.env.DB_COP_OPERATION_AUTHENTICATOR_USERNAME || 'authenticatoroperation',
+  'name': process.env.DB_COP_OPERATION_DBNAME || 'transaction',
+  'username': process.env.DB_COP_OPERATION_AUTHENTICATOR_USERNAME || 'authenticator',
   'password': process.env.DB_COP_OPERATION_AUTHENTICATOR_PASSWORD || 'auth1234',
 };
 
 const keycloak = {
   'url': process.env.KEYCLOAK_URL || 'keycloak.lodev.xyz',
-  'protocol': process.env.KEYCLOAK_PROTOCOL || 'http://',
-  'realm': process.env.KEYCLOAK_REALM || 'dev',
+  'protocol': process.env.KEYCLOAK_PROTOCOL || 'https://',
+  'realm': process.env.KEYCLOAK_REALM || 'cop-local',
 };
 
 const config = {
   'logLevel': process.env.API_COP_LOG_LEVEL || 'info',
 
   'dbConnectionString': process.env.DB_COP_CONNECTION_STRING || `${db.protocol}${db.username}:${db.password}@${db.hostname}:${db.port}/${db.name}${db.options}`,
-  'searchSchema': process.env.DB_COP_OPERATION_SCHEMA || 'operation',
+  'searchSchema': process.env.DB_COP_OPERATION_SCHEMA || 'transaction',
 
   'iss': process.env.ISS || `${keycloak.protocol}${keycloak.url}/realms/${keycloak.realm}`,
-  'keycloak_client_id': process.env.API_COP_KEYCLOAK_CLIENT_ID || 'operational-data-api',
+  'keycloak_client_id': process.env.API_COP_KEYCLOAK_CLIENT_ID || 'api-cop',
   'port': process.env.API_COP_PORT || '5000',
 };
 
