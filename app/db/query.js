4c3d0c7eeb368614ecf808dd2ca0ab30544cdb5c
Pedro Miguel <pedro.miguel@live.co.uk>
2019-09-22 22:39:14 +0100

    Remove deprecated query function

--- app/db/query.js
+++ app/db/query.js
@@ -2,23 +2,6 @@
 const logger = require('../config/logger')(__filename);
 const pool = require('./index');
 
-const query = (role, name, queryString) => new Promise((resolve, reject) => {
-  pool.query(`SET ROLE ${role};`)
-    .then(() => {
-      logger.info('Running query');
-      logger.debug(`Running query ${queryString}`);
-      return pool.query(`${queryString}`);
-    })
-    .then(data => resolve(data.rows))
-    .catch((error) => {
-      const errorMsg = `Unable to run query in table ${name}`;
-      logger.error(errorMsg);
-      logger.error(error.stack);
-      error.message = errorMsg;
-      reject(new Error(errorMsg));
-    });
-});
-
 const query2 = (role, name, queryString, values) => new Promise((resolve, reject) => {
   pool.query(`SET ROLE ${role};`)
     .then(() => {
@@ -40,4 +23,4 @@ const query2 = (role, name, queryString, values) => new Promise((resolve, reject
     });
 });
 
-module.exports = { query, query2 };
+module.exports = query2;
