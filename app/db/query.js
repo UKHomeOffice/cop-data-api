78a907df8efc8b3fc7fd543f1a56326a662b066d
Pedro Miguel <pedro.miguel@live.co.uk>
2020-01-17 14:18:42 +0000

    Update queries to use getPool method

--- app/db/query.js
+++ app/db/query.js
@@ -1,18 +1,14 @@
 // local imports
 const logger = require('../config/logger')(__filename);
-const pool = require('./index');
+const { getPool } = require('./index');
 
 const query = (role, name, queryString, values) => new Promise((resolve, reject) => {
-  pool.query(`SET ROLE ${role};`)
-    .then(() => {
-      const queryObject = {
-        'text': queryString,
-        values,
-      };
-      logger.info('Running query');
-      logger.debug(`Running query: ${queryString}, values: ${values}`);
-      return pool.query(queryObject);
-    })
+  const pool = getPool(role);
+  const queryObject = { 'text': queryString, values };
+
+  logger.info('Running query');
+  logger.debug(`Running query: ${queryString}, values: ${values}`);
+  pool.query(queryObject)
     .then(data => resolve(data.rows))
     .catch((error) => {
       const errorMsg = `Unable to run query in table ${name}`;
