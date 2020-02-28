891f5b42283065c46b29ad072652fbc70c72066f
Joshua Anderson <joshua@diablitodevelopment.com>
2020-02-28 16:08:48 +0000

    CB-1327: Update linting using prettier

--- app/routes/v2/index.js
+++ app/routes/v2/index.js
@@ -13,7 +13,7 @@ app.get('/:name', (req, res) => {
   const { dbrole } = res.locals.user;
 
   if (name.startsWith('pg_') || Boolean(name.match(/\W/))) {
-    return res.status(400).json({ 'error': 'Invalid entity' });
+    return res.status(400).json({ error: 'Invalid entity' });
   }
 
   logger.debug(`Query parameters received: ${queryParams}`);
@@ -25,7 +25,7 @@ app.get('/:name', (req, res) => {
   const { queryString, values } = queryBuilder({ name, queryParams });
 
   if (!queryString) {
-    return res.status(400).json({ 'error': 'Invalid query parameters' });
+    return res.status(400).json({ error: 'Invalid query parameters' });
   }
 
   const data = query(dbrole, name, queryString, values);
@@ -34,7 +34,7 @@ app.get('/:name', (req, res) => {
     .then(resultsArray => res.status(200).json(resultsArray[0]))
     .catch((error) => {
       logger.error(error.stack);
-      res.status(400).json({ 'error': error.message });
+      res.status(400).json({ error: error.message });
     });
 });
 
@@ -44,27 +44,32 @@ app.patch('/:name/:id?', (req, res) => {
   const { prefer } = req.headers;
   const { id, name } = req.params;
   let queryParams = req.query;
-  queryParams = id ? { 'filter': `id=eq.${id}` } : queryParams;
+  queryParams = id ? { filter: `id=eq.${id}` } : queryParams;
   queryParams.update = true;
 
   logger.debug(`Body received: ${JSON.stringify(body)}`);
 
   if (Object.entries(body).length === 0 && (!id || !queryParams)) {
-    return res.status(400).json({ 'error': 'Invalid patch request' });
+    return res.status(400).json({ error: 'Invalid patch request' });
   }
 
   if (queryParams.filter && !Array.isArray(queryParams.filter)) {
     queryParams.filter = [queryParams.filter];
   }
 
-  const { queryString, values } = queryBuilder({ body, name, prefer, queryParams });
+  const { queryString, values } = queryBuilder({
+    body,
+    name,
+    prefer,
+    queryParams,
+  });
   const data = query(dbrole, name, queryString, values);
 
   Promise.all([data])
     .then(resultsArray => res.status(200).json(resultsArray[0]))
     .catch((error) => {
       logger.error(error.stack);
-      res.status(400).json({ 'error': error.message });
+      res.status(400).json({ error: error.message });
     });
 });
 
@@ -83,7 +88,7 @@ app.delete('/:name', (req, res) => {
   const { queryString, values } = queryBuilder({ name, queryParams });
 
   if (!queryString) {
-    return res.status(400).json({ 'error': 'Invalid query parameters' });
+    return res.status(400).json({ error: 'Invalid query parameters' });
   }
 
   const data = query(dbrole, name, queryString, values);
@@ -92,7 +97,7 @@ app.delete('/:name', (req, res) => {
     .then(resultsArray => res.status(200).json(resultsArray[0]))
     .catch((error) => {
       logger.error(error.stack);
-      res.status(400).json({ 'error': error.message });
+      res.status(400).json({ error: error.message });
     });
 });
 
